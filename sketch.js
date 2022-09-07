b7b1191098bbab35002fa0df5358f1bf0025014e
DavidGomez3 <71481274+DavidGomez3@users.noreply.github.com>
2022-09-07 02:47:23 -0500

    Terminado Traslacion y rectangulo y cuadrado

--- sketch.js
+++ sketch.js
@@ -1,26 +1,26 @@
-//Tamaño del canvas
-canvasSize = 650
-
 //Puntos DDA
 var X1 = 0, Y1, X2,Y2, dx, dy;
 
 //Funcion que define estados iniciales
 function setup() {
-  createCanvas(canvasSize, canvasSize);
-  frameRate(12);
-  background(204);
+  var c = createCanvas(600, 600);
+  frameRate(16);
+  background(240);
+  c.mousePressed(click);
 }
 
 //Funcion que se actualiza en tiempo real
 function draw() {
   //Rectangulo(X1, Y1, X2, Y2);
+  //console.log("X: ", mouseX, " Y: ",mouseY);
 }
 
 //Rectangle
 function Rectangulo(X1, Y1, X2, Y2){
+  actualizar();
   X3 = X1;
   Y3 = Y2;
-
+  
   X4 = X2;
   Y4 = Y1;
   
@@ -32,6 +32,7 @@ function Rectangulo(X1, Y1, X2, Y2){
 
 //cuadrado
 function Cuadrado(X1, Y1, X2, Y2){
+  actualizar();
   diferencia = X2 - X1;
   
   nX2 = X1 + diferencia
@@ -70,45 +71,54 @@ function DDA(X1, Y1, X2, Y2){
 }
 
 //Input por clicks
-function mouseClicked(){
-    
-    if (X1 == 0){
+function click(){
+    if (!(document.getElementById("Trasladar").checked) && mouseX <= 600 && mouseX >= 0  && mouseY <= 600 && mouseY >=0){
       X1 = mouseX;
       Y1 = mouseY;
       fill(0);
+      ellipse(X1, Y1, 5, 5);
       console.log("Punto 1 = X:", X1, " Y", Y1);
-    }else{
+    }else if(document.getElementById("Trasladar").checked){
       actualizar();
       traslacion();
     }
   }
   
-  function doubleClicked(){
-    X2 = mouseX;
-    Y2 = mouseY;
-    fill(0);
-    console.log("Punto 2 = X:", X2, " Y", Y2);
+  function mouseReleased(){
+    if (!(document.getElementById("Trasladar").checked) && mouseX <= 600 && mouseX >= 0  && mouseY <= 600 && mouseY >=0) {
+      X2 = mouseX;
+      Y2 = mouseY;
+      fill(0);
+      ellipse(X2, Y2,5, 5);
+      Cuadrado(X1, Y1, X2, Y2);
+      console.log("Punto 2 = X:", X2, " Y", Y2);
+
+    }
 }
 
 //actualizar canvas
 function actualizar(){
+  background(240);
   ellipse(X1, Y1, 5, 5);
   ellipse(X2, Y2,5, 5);
-  background(205);
 }
 
 //traslacion 
 function traslacion() {
+    
   TX = mouseX;
   TY = mouseY;
 
   diferenciaX = TX - X1;
   diferenciaY = TY - Y1;
-  console.log(diferenciaX, diferenciaY);
-  console.log(TX, TY);
-  newX1 = X1 + diferenciaX;
-  newY1 = Y1 + diferenciaY;
-  newX2 = X2 + diferenciaX;
-  newY2 = Y2 + diferenciaY;
-  Rectangulo(newX1, newY1, newX2, newY2) 
+
+  X1 = X1 + diferenciaX;
+  Y1 = Y1 + diferenciaY;
+  X2 = X2 + diferenciaX;
+  Y2 = Y2 + diferenciaY;
+  Cuadrado(X1, Y1, X2, Y2);
+}
+
+function floodFill(){
+  
 }
